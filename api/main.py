78a251e28b3fd2695291cffdd7b6e129affad5a1
ccrmm888 <pagamas.thon@bumail.net>
2025-11-01 11:09:36 +0700

    main,word random

--- api/main.py
+++ api/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from app.schemas import WordResponse
+from fastapi import HTTPException
+from app.routers import words
 
 # Initialize FastAPI app
 app = FastAPI(
@@ -7,15 +10,28 @@ app = FastAPI(
     description="API for vocabulary practice and learning"
 )
 
-@app.get("/api/word")
+@app.get("/api/word", response_model=WordResponse)
 def get_random_word():
     """Get a random word"""
     # TODO Write logic here....
-    return {
+    """return {
         "word": "example",
         "definition": "a representative form or pattern",
-        "difficulty_level": "Beginner"
+        "difficulty_level": "Beginner",
+
     }
+    return WordResponse(
+        id=2,
+        word="Book",
+        definition="This is a book",
+        difficulty_level="Beginner"
+    )"""
+    words = []
+    if len(words) == 0:
+        raise HTTPException(
+            status_code=404,
+            detail="No words available in database...."
+        )
 
 @app.get("/")
 def read_root():
@@ -28,4 +44,6 @@ def read_root():
             "summary": "/api/summary",
             "history": "/api/history"
         }
-    }
+    } 
+
+app.include_router(words.router, prefix="/api", tags=["words"])
